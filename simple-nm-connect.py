24d1cbd4e4708725622c3b2027910681da342609
phil <phil@hattwick.com>
2021-09-16 10:56:34 -0500

    still needs black to format

---
+++ simple-nm-connect.py
@@ -0,0 +1,5 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+net_connect = ConnectHandler(device_type="cisco_ios", host="cisco3.lasthop.io", username="pyclass", password=getpass())
+print(net_connect.find_prompt())
